fe1a7d16d1ee2458a030f8534fde56f3a4f2403e
unknown <pongsakorn.teem@hotmail.com>
2021-02-17 22:17:44 +0700

    fix template daily  .js

--- FrontEnd/testblog/daily.js
+++ FrontEnd/testblog/daily.js
@@ -1,11 +1,19 @@
 Vue.component('dailyblogdate',{
-    template : '<div> <span class="time">{{shortmonth}}<br>{{date}}</span> <p> <em><a href="index.html">The place holder</a></em> so you can see what the site would look like. This is just a place holder </p> </div>' 
+    template : '<div> <span class="time"> {{mounthShort}} <br> {{numDate}} </span> <p> <em><a href="index.html">The place holder</a></em> so you can see what the site would look like. This is just a place holder </p> </div>' 
 })
 
 new Vue({
     el:"#daily-blog",
     data : {
-        date : "17",
-        shortmonth : "Mar"
+        numDate : '17',
+        mounthShort : 'Mar'
+    },
+    method : {
+        getNumDate:function(){
+            return this.numDate
+        },
+        getMonthShort:function(){
+            return this.monthShort
+        }
     }
 })
